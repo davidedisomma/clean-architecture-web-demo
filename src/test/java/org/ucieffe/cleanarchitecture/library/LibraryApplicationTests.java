c10ee7ed79abd73721b3a7e89fda86b6235078b5
Davide Di Somma <davide.disomma@gmail.com>
2022-03-19 00:40:38 +0100

    Setup project

---
+++ src/test/java/org/ucieffe/cleanarchitecture/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ucieffe.cleanarchitecture.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
